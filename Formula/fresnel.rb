ccda383ca49bf023892c4a4e39d07987673edf8d
darkwebber <siddhutime1@gmail.com>
2026-09-04 17:00:30 +0530

    fresnel 0.4.1

--- Formula/fresnel.rb
+++ Formula/fresnel.rb
@@ -3,8 +3,8 @@ class Fresnel < Formula
 
   desc "Mac-native orchestration harness for bounded local coding agents"
   homepage "https://github.com/darkwebber/fresnel"
-  url "https://github.com/darkwebber/fresnel/releases/download/v0.4.0/fresnel_agent-0.4.0.tar.gz"
-  sha256 "78c56b2f408404a5277cef620d9c1054f6922b19bd70ba1a872647adf0c5df7d"
+  url "https://github.com/darkwebber/fresnel/releases/download/v0.4.1/fresnel_agent-0.4.1.tar.gz"
+  sha256 "fa883a73265b0ebdc62a208c6f53635dfce9e70d9e2c17c8631e8fec8701ca91"
   license "Apache-2.0"
 
   depends_on arch: :arm64
@@ -18,6 +18,6 @@ class Fresnel < Formula
   end
 
   test do
-    assert_match "Fresnel 0.4.0", shell_output("#{bin}/fresnel --version")
+    assert_match "Fresnel 0.4.1", shell_output("#{bin}/fresnel --version")
   end
 end
